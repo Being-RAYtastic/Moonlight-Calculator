fccd7d57f36ddb46b03ee58962b50f91bfc76987
Ray ⚡ <103839857+Being-RAYtastic@users.noreply.github.com>
2023-06-06 13:12:45 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+// Your Firebase Crendentials Here
+const FIREBASE_API_KEY = ""
+
+const FIREBASE_AUTH_DOMAIN = ""
+
+const FIREBASE_DATABASE_URL = "
+
+const FIREBASE_PROJECT_ID = "moonlight-calculator"
+
+const FIREBASE_STORAGE_BUCKET = ""
+
+const MESSAGING_SENDER_ID = ""
+
+const FIREBASE_APP_ID = ""
+
+const MEASUREMENT_ID = ""
